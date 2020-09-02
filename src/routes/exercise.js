0cd4c751517c572f808362992495f6b265620d24
sergeialimov <8912652@gmail.com>
2020-09-02 22:10:40 +0500

    add draft of exercise endpoint

--- src/routes/exercise.js
+++ src/routes/exercise.js
@@ -2,13 +2,18 @@ export default {
   method: 'POST',
   path: '/api/exercise/add-new',
   handler: async (request, hapi) => {
-
-    const updatedPass = await UserService.updateUserPassword({ newPass, email });
+    const { userId, description, duration, date } = request.payload;
 
     return hapi.response({
       statusCode: 200,
-      payload: { password: { updated: true } },
-      message: updatedPass.message,
+      payload: {
+        _id: userId,
+        username,
+        date,
+        duration,
+        description,
+      },
+      message: 'New exercise was successfuly added',
     }).code(200);
   },
   options: {
