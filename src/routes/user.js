2cc49489fdd2add1225e9d6dcb9504ee6e2b1359
sergeialimov <8912652@gmail.com>
2020-10-02 14:48:20 +0300

    update query format

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,7 @@ export default {
   method: 'POST',
   path: '/api/exercise/new-user',
   handler: async (request, hapi) => {
-    const { username } = request;
-
+    const { username } = request.query;
     return hapi.response({
       username,
       _id: 1,
