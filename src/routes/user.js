92d84429d5258a290e800d0b971d48bcfbb3e91f
sergeialimov <8912652@gmail.com>
2020-10-02 14:42:12 +0300

    update format of the user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,11 @@ export default {
   method: 'POST',
   path: '/api/exercise/new-user',
   handler: async (request, hapi) => {
-    const { username } = request.payload;
+    const { username } = request;
 
     return hapi.response({
-      statusCode: 200,
-      payload: { username, id: '1' },
+      username,
+      _id: 1,
       message: 'User successfuly created',
     }).code(200);
   },
