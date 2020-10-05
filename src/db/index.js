58b08f7ced0a30c66ba6a882a3f2f33e3b3db142
sergeialimov <8912652@gmail.com>
2020-10-05 18:39:46 +0300

    implement import of models

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,8 @@
-import Sequelize from 'sequelize';
+import { Sequelize, DataTypes } from 'sequelize';
+import path from 'path';
+import fs from 'fs';
+
+const MODELS_PATH = path.join(__dirname, 'models');
 
 const DB = async () => {
   const sequelize = new Sequelize('tracker', 'sa', '', {
@@ -12,7 +16,31 @@ const DB = async () => {
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   };
-  return sequelize;
+
+  // (async () => {
+  //   await sequelize.sync({ force: true });
+  //   // Code here
+  // })();
+
+  const models = {};
+  fs.readdirSync(MODELS_PATH).forEach((file) => {
+    // const model = sequelize.import(path.join(MODELS_PATH, file));
+    const model = require(path.join(MODELS_PATH, file))(sequelize, DataTypes);
+    models[model.name] = model;
+  });
+
+  // eslint-disable-next-line guard-for-in, no-restricted-syntax
+  for (const name in models) {
+    const model = models[name];
+    if (model.associate) {
+      model.associate(models);
+    }
+  }
+
+  return {
+    sequelize,
+    models,
+  };
 };
 
 export default DB;
