a67ec14cf31c9cae82340cbec18b2fc5d526df16
sergeialimov <8912652@gmail.com>
2020-10-05 18:40:13 +0300

    update structure of models

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     _id: {
-      type: Sequelize.INTEGER,
+      type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true
     },
     name: {
-      type: Sequelize.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
       unique: true
     }
