209c03ee825ecfa9d9e9c445bb21ad1a42643b5c
sergeialimov <8912652@gmail.com>
2020-10-11 14:04:03 +0300

    rework importing models

--- src/db/index.js
+++ src/db/index.js
@@ -1,46 +1,21 @@
-import { Sequelize, DataTypes } from 'sequelize';
-import path from 'path';
-import fs from 'fs';
+import { Sequelize } from 'sequelize';
 
-const MODELS_PATH = path.join(__dirname, 'models');
+const sequelize = new Sequelize('tracker', 'sa', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
 
-const DB = async () => {
-  const sequelize = new Sequelize('tracker', 'sa', '', {
-    host: 'localhost',
-    dialect: 'postgres',
-  });
+// (async () => {
+//   await sequelize.sync({ force: true });
+// })();
 
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  };
+const modelDefiners = [
+  require('./models/User.js'),
+];
 
-  // (async () => {
-  //   await sequelize.sync({ force: true });
-  //   // Code here
-  // })();
+for (const modelDefiner of modelDefiners) {
+	modelDefiner(sequelize);
+}
 
-  const models = {};
-  fs.readdirSync(MODELS_PATH).forEach((file) => {
-    // const model = sequelize.import(path.join(MODELS_PATH, file));
-    const model = require(path.join(MODELS_PATH, file))(sequelize, DataTypes);
-    models[model.name] = model;
-  });
 
-  // eslint-disable-next-line guard-for-in, no-restricted-syntax
-  for (const name in models) {
-    const model = models[name];
-    if (model.associate) {
-      model.associate(models);
-    }
-  }
-
-  return {
-    sequelize,
-    models,
-  };
-};
-
-export default DB;
+export default sequelize;
