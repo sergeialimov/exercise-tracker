2ec8fdded88308bba83bb74efbf0ef7648aab9ec
sergeialimov <8912652@gmail.com>
2020-10-11 14:06:08 +0300

    add user creation

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,16 @@
+const sequelize = require('../db/index.js');
+const { User } = sequelize.default.models;
+
 export default {
   method: 'POST',
   path: '/api/exercise/new-user',
   handler: async (request, hapi) => {
     const { username } = request.query;
+
+    await User.create({
+      username,
+    })
+
     return hapi.response({
       username,
       _id: Math.random().toString(36).substring(7),
