9e7f45796c382015f4709c81be11185ebb260d1c
sergeialimov <8912652@gmail.com>
2020-10-12 13:29:42 +0300

    add log route

---
+++ src/routes/log.js
@@ -0,0 +1,23 @@
+const sequelize = require('../db/index.js');
+const { Exercise } = sequelize.default.models;
+
+export default {
+  method: 'POST',
+  path: '/api/exercise/log',
+  handler: async (request, hapi) => {
+    try {
+      const exercises = await Exercise.findAll({
+        where: {
+          userId: request.query.userId,
+        },
+      });
+      return hapi.response(exercises).code(200);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  options: {
+    description: 'Find exercises of user',
+    tags: [ 'api' ],
+  },
+};
